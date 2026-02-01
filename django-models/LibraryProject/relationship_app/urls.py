2a3f3776faf6b6efd460bc0e83b65b2303a09430
Job Oaikhenah <oaikhenahjob@gmail.com>
2026-02-01 23:11:12 +0100

    register logout view

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -8,7 +8,9 @@ This module defines URL patterns for:
 """
 
 from django.urls import path
-from .views import list_books, LibraryDetailView, register, user_login, user_logout
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
+from .views import list_books, LibraryDetailView
 
 urlpatterns = [
     # Function-based view to list all books
@@ -18,7 +20,7 @@ urlpatterns = [
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
     
     # Authentication URLs
-    path('login/', user_login, name='login'),
-    path('logout/', user_logout, name='logout'),
-    path('register/', register, name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
 ]
